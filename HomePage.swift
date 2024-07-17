f979bee096f53726998b56539fec393e22585056
Rosie <rosiepndit@gmail.com>
2024-07-17 11:36:32 -0700

    My home commit (Also just testing)

--- HomePage.swift
+++ HomePage.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        NavigationStack{
+            ZStack{
+                Image("pinkk")
+                VStack(){
+                    Image("logo")
+                    Text("Red Flag")
+                    .font(.title)
+                }
+                
+            }
+        
+        }
     }
 }
 

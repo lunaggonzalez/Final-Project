b7be026aba691277f1cf20c223e9a8001fcc5767
Rosie <rosiepndit@gmail.com>
2024-07-17 14:13:43 -0700

    Pushing

--- HomePage.swift
+++ HomePage.swift
@@ -14,16 +14,50 @@ struct HomePage: View {
         NavigationStack{
             ZStack{
                 Image("pinkk")
+                
                 VStack(){
+                    
                     Image("logo")
+                        
                     Text("Red Flag")
-                    .font(.title)
+                        .font(.system(size: 48))
+                        .bold()
+                    //Text("Welcome to Red Flag! ")
+                    //Text("This app is here to help YOU track your period!")
+                    .padding()
+                    NavigationLink(destination: CalendarView()) {
+                        Text("Calendar         ")
+                            .bold()
+                            .tint(.purple)
+                            .font(.system(size: 34))
+                            .background(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.purple, lineWidth: 2)
+                                        )
+                            //.border(Color.purple)
+                            
+                    }
+                    
+                    NavigationLink(destination: FAQ()) {
+                        Text("FAQ                   ")
+                            .bold()
+                            .tint(.purple)
+                            .font(.system(size: 34))
+                            .background(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.purple, lineWidth: 2)
+                                        )
+                            //.border(Color.purple)
+                            
+                            
+                    }
+                    .padding()
+                    
                 }
                 
             }
         
-        }
-    }
+        }    }
 }
 
 #Preview {

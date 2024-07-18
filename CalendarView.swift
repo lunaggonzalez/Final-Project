3ec4af45b024f5344412adf84d1f28355e2ccf74
Luna <lunaggonzalez@gmail.com>
2024-07-18 10:26:20 -0700

    calendar view with background

--- CalendarView.swift
+++ CalendarView.swift
@@ -11,14 +11,18 @@ struct CalendarView: View {
     @State var selectedDate = Date()
     
     var body: some View {
-        VStack {
-            Text("Calendar View")
-                .font(.title)
-                .foregroundColor(Color.red)
-            CustomDatePicker(selectedDate: $selectedDate)
-                .padding()
-            Text(selectedDate.formatted(date: .abbreviated, time: .omitted)) // <-- you can customize (or omit it altogether) however you like!
-            Spacer()
+        ZStack{
+            Image("pinkk").resizable().ignoresSafeArea()
+            
+            VStack {
+                Text("Calendar View")
+                    .font(.title)
+                    .foregroundColor(Color.red)
+                CustomDatePicker(selectedDate: $selectedDate)
+                    .padding()
+                Text(selectedDate.formatted(date: .abbreviated, time: .omitted)) // <-- you can customize (or omit it altogether) however you like!
+                Spacer()
+            }
         }
     }
 }
